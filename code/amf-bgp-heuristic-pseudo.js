56fafb8e480cae870490523edd0460e8f249dbbe
Deployment Bot (from Travis CI) <deploy@travis-ci.org>
2019-04-04 13:29:53 +0000

    Deploy comunica/Article-ISWC2019-AMF to github.com/comunica/Article-ISWC2019-AMF.git:gh-pages

--- code/amf-bgp-heuristic-pseudo.js
+++
@@ -1,6 +0,0 @@
-function preferAmfForBgp(bindingsCount, triplePatternMatches) {
-  totalAmfsSize = triplePatternMatches.sum() * AMF_TRIPLE_SIZE;
-  joinRequestData = (bindingsCount * triplePatternMatches.length)
-      * TPF_BINDING_SIZE;
-  return totalAmfsSize < joinRequestData;
-}
